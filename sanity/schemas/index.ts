7d853105e8b19d366a5628667a171d89519e9de7
elpatryk <patrykzmurko@yahoo.com>
2023-01-26 16:59:01 +0100

    setting up sanity

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,7 @@
+import pageInfo from './pageInfo'
+import skill from './skill'
+import social from './social'
+import project from './project'
+import experience from './experience'
+
+export const schemaTypes = [pageInfo, skill, social, project, experience]
