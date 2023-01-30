72a797fdc517137b8bff73300ae5ef4f1a6d9452
elpatryk <patrykzmurko@yahoo.com>
2023-01-30 17:39:55 +0100

    doesnt work

--- sanity.ts
+++ sanity.ts
@@ -5,7 +5,7 @@ export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET! || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
 
-  apiVersion: "v1",
+  apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
 };
 
