0a18545f976b4515e9ff575f5c87989fcee9fb8c
elpatryk <patrykzmurko@yahoo.com>
2023-01-30 17:26:34 +0100

    doesnt work

--- sanity.ts
+++ sanity.ts
@@ -4,7 +4,7 @@ import createImageUrlBuilder from "@sanity/image-url";
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET! || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  token: "XMCNHCWLFnlm0sjQdFrQYd02",
+
   apiVersion: "v1",
   useCdn: process.env.NODE_ENV === "production",
 };
