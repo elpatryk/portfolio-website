0262b6bb93d5b7a492f676ab52c0dd5290ca89bb
elpatryk <patrykzmurko@yahoo.com>
2023-01-30 17:42:26 +0100

    doesnt work

--- sanity.ts
+++ sanity.ts
@@ -6,7 +6,6 @@ export const config = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
 
   apiVersion: "2021-03-25",
-  useCdn: process.env.NODE_ENV === "production",
 };
 
 export const sanityClient = createClient(config);
