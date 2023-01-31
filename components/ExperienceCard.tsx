115a416cb57d1b2fdaad5d3bd6fddba95df9d488
elpatryk <patrykzmurko@yahoo.com>
2023-01-31 14:38:48 +0100

    doesnt work

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -11,7 +11,7 @@ type Props = {
 export default function ExperienceCard({ experience }: Props) {
   return (
     <article
-      className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40
+      className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40
     cursor-pointer  transition-opacity duration-200 overflow-hidden"
     >
       <motion.img
