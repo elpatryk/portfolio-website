aaec65e3808438636185946c9f09554924a3fd48
elpatryk <patrykzmurko@yahoo.com>
2023-01-31 11:11:25 +0100

    doesnt work

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -1,7 +1,9 @@
 import { Experience } from "@/typings";
 
 export const fetchExperience = async () => {
-  const res = await fetch(`http://localhost:3000/api/getExperiences`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`
+  );
 
   const data = await res.json();
   const experiences: Experience[] = data.experiences;
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,7 +1,9 @@
 import { Project } from "@/typings";
 
 export const fetchProjects = async () => {
-  const res = await fetch(`http://localhost:3000/api/getProjects`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`
+  );
 
   const data = await res.json();
   const projects: Project[] = data.projects;
