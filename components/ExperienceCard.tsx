a94585587c3535a882a757ad531c83bd662af596
elpatryk <patrykzmurko@yahoo.com>
2023-01-31 15:53:50 +0100

    doesnt work

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -29,8 +29,8 @@ export default function ExperienceCard({ experience }: Props) {
         alt=""
       />
       <div className="px-0 md:px-10">
-        <h4 className="text-4xl font-light">Student</h4>
-        <p className="font-bold text-2xl mt-1">Codaisseur Academy</p>
+        <h4 className="text-4xl font-light">{experience.jobTitle}</h4>
+        <p className="font-bold text-2xl mt-1">{experience.company}</p>
         {/* <div className="flex space-x-2 my-2 ">
           {experience?.technologies.map((technology, i) => (
             <img
